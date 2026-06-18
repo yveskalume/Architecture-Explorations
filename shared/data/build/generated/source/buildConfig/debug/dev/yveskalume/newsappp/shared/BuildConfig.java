2cac249b7f234410dec9584e8c6152c24c56a707
Yves Kalume <55670723+yveskalume@users.noreply.github.com>
2026-06-18 20:22:48 +0200

    create a shared gradle subproject for data and co

---
+++ shared/data/build/generated/source/buildConfig/debug/dev/yveskalume/newsappp/shared/BuildConfig.java
@@ -0,0 +1,11 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package dev.yveskalume.newsappp.shared;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "dev.yveskalume.newsappp.shared";
+  public static final String BUILD_TYPE = "debug";
+  public static final String NEWS_API_KEY = "ec331198bd3548738760a591155484e8";
+}
